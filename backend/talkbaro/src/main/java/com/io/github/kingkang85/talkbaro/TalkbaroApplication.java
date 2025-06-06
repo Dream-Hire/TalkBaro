91472e2ed6ea0a3d69bf274787b1208071576261
강지민 <kjmemail2743@gmail.com>
2025-06-06 19:11:07 +0900

    init: 백엔드 초기 세팅

---
+++ backend/talkbaro/src/main/java/com/io/github/kingkang85/talkbaro/TalkbaroApplication.java
@@ -0,0 +1,13 @@
+package com.io.github.kingkang85.talkbaro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkbaroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkbaroApplication.class, args);
+	}
+
+}
---
+++ backend/talkbaro/src/test/java/com/io/github/kingkang85/talkbaro/TalkbaroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.io.github.kingkang85.talkbaro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkbaroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
